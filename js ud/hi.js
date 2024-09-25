393e01c53d23173268da62776ef47fdc76291014
MadhavaRahul <149048809+MadhavaRahul@users.noreply.github.com>
2024-09-25 23:27:27 +0530

    Add files via upload

---
+++ js ud/hi.js
@@ -0,0 +1,6 @@
+var a="<h1>Hi! Roger.</h1>"
+document.write(a)
+document.write("<i>I'm learning js.Captain</i>")    
+// js writes txt into webpg
+console.log("It's Hidden msg. cap!")                
+// js writes into console
